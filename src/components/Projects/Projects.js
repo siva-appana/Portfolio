4248c1eb01f0d35b42527faf7a9ed47e16d242d7
Siva Appana <sappana2003@outlook.com>
2023-02-10 00:58:23 -0500

    Under construction note

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import freeflight from "../../Assets/Projects/freeflight.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -18,71 +13,17 @@ function Projects() {
           My Recent <strong className="purple">Projects </strong>
         </h1>
         <p style={{ color: "white" }}>
-          The following cards include projects I have recently completed or projects currently in progress.
+          The following cards include projects I have recently completed or projects currently in progress. Note: THIS PAGE IS CURRENTLY UNDER CONSTRUCTION.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={freeflight}
               isBlog={false}
               title="Rubber-band Powered Endurance Plane"
-              description="For free flight competitions, I constructed several self-designed rubber-band planes using balsa wood and 1.4 micron mylar covering. My highest flight time was 2.5 minutes of sustained flight, which won 2nd place in the 2021 TSA Flight Endurance competition among several medals earned in custom models constructed for Science Olympiad. Linked below is a video from my first free flight competition."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              description="For free flight competitions, I researched and constructed several self-designed rubber-band planes using balsa wood and 1.4 micron mylar covering. My highest flight time was 2.5 minutes of sustained flight, which won 2nd place in the 2021 TSA Flight Endurance competition among several medals earned in custom models constructed for Science Olympiad. The links below include a detailed documentation of my final flight and a video from my first free flight competition."
+              ghLink="https://raw.githubusercontent.com/siva-appana/Portfolio/main/src/Assets/Projects/Flight_Documentation_Siva_Appana.pdf"
+              demoLink="https://raw.githubusercontent.com/siva-appana/Portfolio/main/src/Assets/Projects/freeflight-video.mp4"
             />
           </Col>
         </Row>
